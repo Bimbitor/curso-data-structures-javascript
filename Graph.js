ddc31c22e015fd0f8b68d2b9c3baf9143e66e0bb
Bimbitor <javijohanro30@gmail.com>
2023-04-23 22:46:04 -0500

    Finished!!!!!

--- Graph.js
+++ Graph.js
@@ -3,14 +3,37 @@ class Graph {
         this.nodes = 0;
         this.adjacentList = {};
     }
-    addVertex(node) {
+    addVertice(node) {
         this.adjacentList[node] = [];
         this.nodes++;
     }
-    addEdge(node1, node2) {
+    addConex(node1, node2) {
         this.adjacentList[node1].push(node2);
         this.adjacentList[node2].push(node1);
+    }
+    check(){
+        return this
     }
 }
 
 const myGraph = new Graph();
+
+
+// Vértices
+myGraph.addVertice(1);
+myGraph.addVertice(3);
+myGraph.addVertice(4);
+myGraph.addVertice(5);
+myGraph.addVertice(6);
+myGraph.addVertice(8);
+
+// Conexiones
+myGraph.addConex(8,4)
+myGraph.addConex(4,1)
+myGraph.addConex(1,3)
+myGraph.addConex(1,6)
+myGraph.addConex(3,6)
+myGraph.addConex(3,5)
+myGraph.addConex(5,4)
+
+console.log(myGraph.check());
